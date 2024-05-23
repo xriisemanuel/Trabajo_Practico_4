00b34221ac15f7391f7c09527ff9237dadb17958
xriis <xriis@x7i>
2024-05-23 14:45:01 -0300

    index.html agregado y creando las clases...

--- tp4_AmadorCristianEmanuel/src/main/java/ar/edu/unju/fi/model/Docente.java
+++ tp4_AmadorCristianEmanuel/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -1,5 +1,11 @@
 package ar.edu.unju.fi.model;
 
 public class Docente {
-
+	private int legajo;
+	private String name;
+	private String lastname;
+	private String email;
+	private String phone;
+	
+	
 }
