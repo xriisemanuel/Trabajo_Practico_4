2e7d6d1855c11743056d1124b16813e4ba9f8823
xriis <xriis@x7i>
2024-05-23 15:51:43 -0300

    pruebas de implementacion de Materialize

--- tp4_AmadorCristianEmanuel/src/main/java/ar/edu/unju/fi/controller/IndexController.java
+++ tp4_AmadorCristianEmanuel/src/main/java/ar/edu/unju/fi/controller/IndexController.java
@@ -2,10 +2,12 @@ package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/principal") /*si o si se debe escribir: localhost:8080/principal/*/
 public class IndexController {
-	@GetMapping("/principal")
+	@GetMapping({"/index","/home"})/*../principal/index*/
 	public String getIndexPage() {
 		return "index";
 	}
--- tp4_AmadorCristianEmanuel/src/main/java/ar/edu/unju/fi/model/Docente.java
+++ tp4_AmadorCristianEmanuel/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -6,6 +6,57 @@ public class Docente {
 	private String lastname;
 	private String email;
 	private String phone;
-	
-	
+
+	public Docente() {
+	}
+
+	public Docente(int legajo, String name, String lastname, String email, String phone) {
+		super();
+		this.legajo = legajo;
+		this.name = name;
+		this.lastname = lastname;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public int getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(int legajo) {
+		this.legajo = legajo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
 }
